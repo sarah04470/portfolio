cb2aab3268dc78e1f3a9e47598a04dab8a391eca
황선하 <hwangseonha@hwangseonhaui-MacBookPro.local>
2023-08-14 14:50:21 +0900

    reset.css

--- src/styles/common/header.styles.js
+++ src/styles/common/header.styles.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
 
 export const HeaderWrap = styled.header`
-    display: flex;
+    min-height: 850px; width: 100%; height: 50px; padding: 0 20px; background-color: #e5effa; position: fixed; z-index: 2;
 `;
